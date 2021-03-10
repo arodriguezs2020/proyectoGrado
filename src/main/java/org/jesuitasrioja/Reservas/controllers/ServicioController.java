b5de9e4d9e192cf73e42696a4db1e3c746b18f2e
Alvaro Rodriguez <dam16.2020@gmail.com>
2021-03-10 11:21:52 +0100

    depereeewwweewww

--- src/main/java/org/jesuitasrioja/Reservas/controllers/ServicioController.java
+++ src/main/java/org/jesuitasrioja/Reservas/controllers/ServicioController.java
@@ -1,14 +1,11 @@
 package org.jesuitasrioja.Reservas.controllers;
 
+import java.util.List;
 import java.util.Optional;
-import java.util.function.Function;
 
 import org.jesuitasrioja.Reservas.modelo.servicio.Servicio;
-import org.jesuitasrioja.Reservas.modelo.servicio.ServicioDTO;
-import org.jesuitasrioja.Reservas.modelo.servicio.ServicioDTOConverter;
 import org.jesuitasrioja.Reservas.persistencia.services.ServicioService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
@@ -25,44 +22,31 @@ import io.swagger.annotations.ApiOperation;
 
 @RestController
 public class ServicioController {
-	
+
 	@Autowired
 	private ServicioService ss;
-	
-	@Autowired
-	ServicioDTOConverter servicioDTOConverter;
 
 	/*
 	 * 
 	 * GET servicios
 	 * 
-	 * */
-	
-	@ApiOperation(value = "Obtener todos los servicios paginados",
-			 notes = "Con este metodo conseguimos mandar todos los servicios de 10 en 10. Así la Web podrá recoger los datos mas facilmente.")
+	 */
+
+	@ApiOperation(value = "Obtener todos los servicios paginados", notes = "Con este metodo conseguimos mandar todos los servicios de 10 en 10. Así la Web podrá recoger los datos mas facilmente.")
 	@GetMapping("/servicios")
 	public ResponseEntity<?> allServicios(@PageableDefault(size = 10, page = 0) Pageable pageable) {
-		Page<Servicio> pagina = ss.findAll(pageable);
-		
-		// transformar elementos de la pagina a DTO
-				Page<ServicioDTO> paginaDTO = pagina.map(new Function<Servicio, ServicioDTO>() {
-					@Override
-					public ServicioDTO apply(Servicio s) {
-						return servicioDTOConverter.convertServicioToServicioDTO(s);
-					}
-				});
+		List<Servicio> servicios = ss.findAll();
 
-		return ResponseEntity.status(HttpStatus.OK).body(paginaDTO);
+		return ResponseEntity.status(HttpStatus.OK).body(servicios);
 	}
-	
+
 	/*
 	 * 
 	 * GET servicio/{idServicio}
 	 * 
-	 * */
-	
-	@ApiOperation(value = "Obtener un servicio por identificador",
-			 notes = "Con este metodo conseguimos recoger la información de un servicio específico.")
+	 */
+
+	@ApiOperation(value = "Obtener un servicio por identificador", notes = "Con este metodo conseguimos recoger la información de un servicio específico.")
 	@GetMapping("/servicio/{id}")
 	public ResponseEntity<Servicio> getServicio(@PathVariable Integer id) {
 
@@ -73,41 +57,38 @@ public class ServicioController {
 			throw new ServicioNoEncontradoException(id);
 		}
 	}
-	
+
 	/*
 	 * 
 	 * POST servicio
 	 * 
-	 * */
-	
-	@ApiOperation(value = "Añadir un servicio",
-			 notes = "Con este metodo conseguimos añadir un servicio.")
+	 */
+
+	@ApiOperation(value = "Añadir un servicio", notes = "Con este metodo conseguimos añadir un servicio.")
 	@PostMapping("/servicio")
 	public String postServicio(@RequestBody Servicio nuevoServicio) {
 		return ss.save(nuevoServicio).toString();
 	}
-	
+
 	/*
 	 * 
 	 * PUT servicio
 	 * 
-	 * */
-	
-	@ApiOperation(value = "Modificar un servicio",
-			 notes = "Con este metodo conseguimos modificar un Servicio.")
+	 */
+
+	@ApiOperation(value = "Modificar un servicio", notes = "Con este metodo conseguimos modificar un Servicio.")
 	@PutMapping("/servicio")
 	public String putServicio(@RequestBody Servicio editadoServicio) {
 		return ss.edit(editadoServicio).toString();
 	}
-	
+
 	/*
 	 * 
 	 * DELETE servicio/{idServicio}
 	 * 
 	 **/
-	
-	@ApiOperation(value = "Borrar un servicio",
-			 notes = "Con este metodo conseguimos borrar un Servicio por identificador. De esta forma conseguiremos borrar un Servicio específico.")
+
+	@ApiOperation(value = "Borrar un servicio", notes = "Con este metodo conseguimos borrar un Servicio por identificador. De esta forma conseguiremos borrar un Servicio específico.")
 	@DeleteMapping("/servicio/{id}")
 	public String deleteServicio(@PathVariable Integer id) {
 		ss.deleteById(id);
